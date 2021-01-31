573773ba236867edeb8ab77ae34b865a7e164502
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-01-31 21:00:35 +0000

    Update rows.js

--- resources/modules/rows.js
+++ resources/modules/rows.js
@@ -1,26 +1,23 @@
-module.exports = 
+exports.colorize = (client) =>
 {
-    async getRows()
-    {
-        // Redefine the 'doc' (sheet) for easier access, initialize Discord embed
-        const doc = this.client.doc;
-        const embed = new this.client.Discord.MessageEmbed();
-        const keyCodes = this.client.keyCodes;
+    // Redefine the 'doc' (sheet) for easier access, initialize Discord embed
+    const doc = this.client.doc;
+    const embed = new this.client.Discord.MessageEmbed();
+    const keyCodes = this.client.keyCodes;
 
-        // Create a connection between the bot and the Google sheet
-        await doc.useServiceAccountAuth(this.client.google);
-        await doc.loadInfo();
+    // Create a connection between the bot and the Google sheet
+    await doc.useServiceAccountAuth(this.client.google);
+    await doc.loadInfo();
 
-        // Automatically find the Catalog sheet. Yay!
-        var sheetId = 0;
-        doc.sheetsByIndex.forEach(x => {
-        if (x.title == "Catalog") sheetId = x.sheetId;
-        });
+    // Automatically find the Catalog sheet. Yay!
+    var sheetId = 0;
+    doc.sheetsByIndex.forEach(x => {
+    if (x.title == "Catalog") sheetId = x.sheetId;
+    });
 
-        // Get the sheet and an obj array containing its rows
-        const sheet = doc.sheetsById[sheetId];
-        const rows = await sheet.getRows();
+    // Get the sheet and an obj array containing its rows
+    const sheet = doc.sheetsById[sheetId];
+    const rows = await sheet.getRows();
 
-        return rows;
-    }
+    return rows;
 };
