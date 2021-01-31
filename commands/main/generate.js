e3a239f56749b63fbcc0f5a1006dfa0333c02e81
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-01-31 20:31:43 +0000

    separate doc loading test

--- commands/main/generate.js
+++ commands/main/generate.js
@@ -64,24 +64,8 @@ module.exports = class extends Command
     //message.say(`${processedArgs}`);
     //message.say(`${numTracks}, ${desiredKey}, ${desiredBPM}, ${desiredGenre}`);
     
-    // Redefine the 'doc' (sheet) for easier access, initialize Discord embed
-    const doc = this.client.doc;
-    const embed = new this.client.Discord.MessageEmbed();
-    const keyCodes = this.client.keyCodes;
-
-    // Create a connection between the bot and the Google sheet
-    await doc.useServiceAccountAuth(this.client.google);
-    await doc.loadInfo();
-
-    // Automatically find the Catalog sheet. Yay!
-    var sheetId = 0;
-    doc.sheetsByIndex.forEach(x => {
-      if (x.title == "Catalog") sheetId = x.sheetId;
-    });
-
-    // Get the sheet and an obj array containing its rows
-    const sheet = doc.sheetsById[sheetId];
-    const rows = await sheet.getRows();
+    getSheet = require('./resources/modules/doc.js');
+    const rows = getSheet();
     
     // Big try/catch purely to spam ping Hanabi when you're debugging a crashing issue
     try
@@ -224,7 +208,7 @@ module.exports = class extends Command
           return track;
         }
       }
-
+      
       //console.log(`Defaulted to track: ${track.Track}. Genre: ${genre}`);
       return track;
     }
