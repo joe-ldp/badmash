1b0e66e850c3fef27e338db3eb427f6bc9315f91
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-02-02 13:19:07 +0000

    Update handler.js

--- resources/modules/handler.js
+++ resources/modules/handler.js
@@ -13,12 +13,16 @@ getCover = async(client, ID) =>
     .then(json => (releaseID = json.release.id))
     .then(async() =>
     {
+      console.log(res);
+      console.log(json.release.id);
       const coverURL = await client.fetch(`https://connect.monstercat.com/v2/release/${releaseID}/cover?image_width=512`);
+      console.log(coverURL);
       coverImage = await coverURL.buffer();
+      console.log(coverImage);
     })
     .catch(err => 
     {
-      console.error(err);
+      //console.error(err);
       coverImage = defaultImage;
     });
 
