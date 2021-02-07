0866c48d1e24e61117d4fa503aed750caffa6aad
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-02-07 19:11:15 +0000

    fix some indentations

--- commands/util/help.js
+++ commands/util/help.js
@@ -4,98 +4,84 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command
 {
-    constructor (client) 
+  constructor (client) 
+  {
+    super(client, 
     {
-        super(client, 
-        {
-            name: 'help',
-            group: 'util',
-            memberName: 'help',
-            aliases: ['h', 'wtf'],
-            description: 'Lists and gives information about available commands.',
-            examples: [`${client.commandPrefix}help`, `${client.commandPrefix}help info`],
-        });
-    }
-  
-    run (message)
-    {
-        // Easy access for the bot avatar
-        const botAvatar = this.client.users.cache.get(process.env.BOT_ID).avatarURL();
-        const args = message.content.slice(this.client.commandPrefix).trim().split(/ +/g)[1];
+      name: 'help',
+      group: 'util',
+      memberName: 'help',
+      aliases: ['h', 'wtf'],
+      description: 'Lists and gives information about available commands.',
+      examples: [`${client.commandPrefix}help`, `${client.commandPrefix}help info`],
+    });
+}
 
-        // --DEBUG-- Induce a fake error
-        //if (args == "h") return await this.client.handler.throw(this.client, message, args);
+run (message)
+{
+  try
+  {
+      const args = message.content.slice(this.client.commandPrefix).trim().split(/ +/g)[1];
+      const prefix = process.env.PREFIX;
+      const embed = new MessageEmbed();
+
+      const { commands } = this.client.registry;
+      
+      const color = this.client.handler.colorize(this.client);
+      
+      const hanabi = this.client.users.cache.get(process.env.OWNER_ID).tag;
+      const ravalle = this.client.users.cache.get(process.env.CO_ID).tag;
+
+      // Master embed content
+      embed
+        .setColor(`${color}`)
+        .setTitle(`MCatalog Bot v${process.env.VERSION}`)
+        .setThumbnail(`${this.client.botAvatar}`)
+        .setFooter(`Brought to you by ${hanabi} and ${ravalle}.`)
+        .setDescription(`Badmash: A Discord bot that fetches data from\nhttps://rebrand.ly/mcatalog\n\n**Prefix: ${prefix}**`)
+        ;
+      
+      var cmdHelp = false;
+      commands.forEach(cmd => 
+      {
+        // Detect any commands specified
+        if ((args == cmd.name || cmd.aliases.indexOf(args) != -1))
+        {
+          // Flag cmdHelp to indicate we want to display specific cmd info
+          cmdHelp = true;
+
+          // Add info to the embed
+          embed.addField(`Command: \`${prefix}${cmd.name}\``, `${cmd.description}`);
+          
+          var aliases = cmd.aliases.join(', '),
+            examples = cmd.examples.join('\n');
+
+          embed
+            .addField(`Aliases:`, `${aliases}`)
+            .addField(`Examples:`, `${examples}`);
+        }
+      });
+      
+      // No command was detected in the user input, display default help message
+      if (!cmdHelp)
+      {
+        embed
+          .addField(`**Command List**`, `Below is a list of commands provided by this bot.`);
         
-        // Initialize args and variables
-        const
-            hanabi = this.client.users.cache.get(process.env.OWNER_ID).tag,
-            ravalle = this.client.users.cache.get(process.env.CO_ID).tag,
-
-            prefix = process.env.prefix,
-            embed = new MessageEmbed(),
-            
-            { commands } = this.client.registry;
-
-        var color = this.client.handler.colorize(this.client);
-        //var data;
-
-        //return console.log(`${JSON.stringify(commands)}`);
-
-        try
+        commands.forEach(cmd =>
         {
-            //data.push(commands.map(cmd => cmd.name).join(', '));
+          if(cmd.groupID != "admin" && cmd.groupID != "commands" && cmd.name != "eval")
+            embed.addField(`${cmd.name}`, `${cmd.description}`);
+        });
+      }
 
-            // Master embed content
-            embed
-                .setColor(`${color}`)
-                .setTitle(`MCatalog Bot v${process.env.VERSION}`)
-                .setThumbnail(`${botAvatar}`)
-                .setFooter(`Brought to you by ${hanabi} and ${ravalle}.`)
-                .setDescription(`Badmash: A Discord bot that fetches data from\nhttps://rebrand.ly/mcatalog\n\n**Prefix: ${prefix}**`)
-                //.addField(`${data}`);
-                ;
-            
-            var cmdHelp = false;
-            commands.forEach(cmd => 
-            {
-                // Detect any commands specified
-                if ((args == cmd.name || cmd.aliases.indexOf(args) != -1))
-                {
-                  // Flag cmdHelp to indicate we want to display specific cmd info
-                  cmdHelp = true;
-
-                  // Add info to the embed
-                  embed.addField(`Command: \`${prefix}${cmd.name}\``, `${cmd.description}`);
-                  
-                  var aliases = cmd.aliases.join(', '),
-                      examples = cmd.examples.join('\n');
-
-                  embed
-                    .addField(`Aliases:`, `${aliases}`)
-                    .addField(`Examples:`, `${examples}`);
-                }
-            });
-            
-            // No command was detected in the user input, display default help message
-            if (!cmdHelp)
-            {
-                embed
-                    .addField(`**Command List**`, `Below is a list of commands provided by this bot.`);
-                
-                commands.forEach(cmd =>
-                {
-                    if(cmd.groupID != "admin" && cmd.groupID != "commands" && cmd.name != "eval")
-                        embed.addField(`${cmd.name}`, `${cmd.description}`);
-                });
-            }
-
-            // Send the embed
-            message.channel.send(embed).catch(console.error);
-        }
-        catch (err)
-        {
-            // Inform bot owner for error, send error log, and log it
-            this.client.handler.throw(this.client, message, err);
-        }
+      // Send the embed
+      message.channel.send(embed);
     }
+    catch (err)
+    {
+      // Inform bot owner for error, send error log, and log it
+      this.client.handler.throw(this.client, message, err);
+    }
+  }
 }
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const genrePrefixes = require('./resources/objects/genrePrefixes.json');
 const contentWarning = require('./resources/objects/contentWarning.json');
 const licensability = require('./resources/objects/licensability.json');
 
+
 var google = require('./resources/keys/google.json');
 google.private_key_id = process.env.GOOGLE_PRIVATE_KEY_ID;
 google.private_key = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n");
@@ -23,10 +24,11 @@ google.private_key = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n");
 // Initialize the Commando client
 const client = new CommandoClient(
 {  
-	commandPrefix: process.env.PREFIX,
-	owner: [process.env.OWNER_ID, process.env.CO_ID],
+  commandPrefix: process.env.PREFIX,
+  owner: [process.env.OWNER_ID, process.env.CO_ID],
   disableEveryone: true,
 });
+
 // Bind dependencies to client for sub-unit usage
 client.Discord = Discord;
 
@@ -42,6 +44,7 @@ client.genrePrefixes = genrePrefixes;
 client.contentWarning = contentWarning;
 client.licensability = licensability;
 
+
 // Initialize Google Sheets API
 const doc = new client.gs(process.env.SHEET_KEY);
 client.doc = doc;
@@ -49,30 +52,30 @@ client.doc = doc;
 // Initialize events
 fs.readdir("./events/", (err, files) =>
 {
-	if (err) return console.error(err);
-	files.forEach(file =>
+  if (err) return console.error(err);
+  files.forEach(file =>
   {
-		const event = require(`./events/${file}`);
-		let eventName = file.split(".")[0];
-		client.on(eventName, event.bind(null, client));
-	});
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
 });
 
 // Initialize commands
 client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['admin', 'Owner-only commands'],
-        ['main', 'Main bot commands'],
-        ['util', 'Utility commands'],
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands({
-        ping: false,
-        help: false,
-        unknownCommand: false
-    })
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+.registerDefaultTypes()
+.registerGroups([
+  ['admin', 'Owner-only commands'],
+  ['main', 'Main bot commands'],
+  ['util', 'Utility commands'],
+])
+.registerDefaultGroups()
+.registerDefaultCommands({
+  ping: false,
+  help: false,
+  unknownCommand: false
+})
+.registerCommandsIn(path.join(__dirname, 'commands'));
 
 // Finally login
 client.login(process.env.BOT_TOKEN);
