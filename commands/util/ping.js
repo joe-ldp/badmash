1244d7e3404bdb85750eb1e348f9e6106feb8884
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-03-16 00:01:28 +0000

    correct indentation; add aliases[] to ping so c.help ping works

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -4,38 +4,39 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command
 {
-    constructor (client) 
+  constructor (client) 
+  {
+    super(client, 
     {
-        super(client, 
-        {
-            name: 'ping',
-            group: 'util',
-            memberName: 'ping',
-            description: "Shows the bot's response time",
-            examples: [`${client.commandPrefix}ping`]
-        });
-    }
-  
-    async run (message)
-    {
-        try
-        {
-			// Initalize Discord embed and required variables
-			const embed = new MessageEmbed();
-			const ping = Date.now() - message.createdTimestamp;
-			const color = this.client.handler.colorize(this.client);
+      name: 'ping',
+      group: 'util',
+      memberName: 'ping',
+      aliases: ['p'],
+      description: "Shows the bot's response time",
+      examples: [`${client.commandPrefix}ping`]
+    });
+  }
 
-			// Build the embed
-			embed
-				.setColor(`${color}`)
-				.setTitle(`:ping_pong: **Pong!** Ping is __${Math.round(ping)}__ ms.`);
-			
-			return message.channel.send(embed).catch(console.error);
-        }
-        catch (err)
-        {
-            // Inform bot owner for error, send error log, and log it
-            await this.client.handler.throw(this.client, err, message);
-        }
+  async run (message)
+  {
+    try
+    {
+      // Initalize Discord embed and required variables
+      const embed = new MessageEmbed();
+      const ping = Date.now() - message.createdTimestamp;
+      const color = this.client.handler.colorize(this.client);
+
+      // Build the embed
+      embed
+        .setColor(`${color}`)
+        .setTitle(`:ping_pong: **Pong!** Ping is __${Math.round(ping)}__ ms.`);
+      
+      return message.channel.send(embed).catch(console.error);
     }
+    catch (err)
+    {
+      // Inform bot owner for error, send error log, and log it
+      await this.client.handler.throw(this.client, err, message);
+    }
+  }
 }
