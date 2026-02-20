510c5381a1b961f875b5aa8dbf21f8f5d6236b5a
Joe <joedkirkup@gmail.com>
2026-02-20 16:43:39 +0000

    Exclude Mirai Sekai tracks
    Known intentional formatting differences on mcatalog

--- commands/admin/licensability.js
+++ commands/admin/licensability.js
@@ -33,6 +33,7 @@ module.exports = {
         try {
             for (const [rowNum, row] of rows.entries()) {
                 if (rowNum < 3) continue;
+                if (row.Track.includes("Mirai Sekai")) continue;
                 if ((percent = Math.round((rowNum / rows.length) * 100)) > lastPercent) {
                     let funcTime = Date.now() - startTime;
                     let timeLeft = (funcTime / rowNum) * (rows.length - rowNum) / 1000;
