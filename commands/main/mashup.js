26338b67fca585c6126acfdae00766c25fce7249
Joe <joedkirkup@gmail.com>
2026-03-04 22:59:16 +0000

    🚧Create first version of /mashup command
    Lots of work to be done still

---
+++ commands/main/mashup.js
@@ -0,0 +1,123 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { Mashup } = require.main.require('./resources/modules/mashup.js');
+const { searchTrack } = require.main.require('./resources/modules/pickTrack.js');
+const { getRows } = require.main.require('./resources/modules/sheet.js');
+const fs = require('fs');
+const mashupsJson = './resources/objects/mashups.json';
+
+module.exports = {
+    cooldown: 10,
+    data: new SlashCommandBuilder()
+        .setName('mashup')
+        .setDescription('Toolkit for building mashup tracklists.')
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('new')
+                .setDescription('Create a new mashup.')
+                .addStringOption(option =>
+                    option.setName('tracks')
+                    .setDescription('Comma-separated list of tracks to add. If none specified, 2 suggested tracks will be added.')
+                    .setRequired(false)),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('list')
+                .setDescription('List all your mashups.'),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('edit')
+                .setDescription('Edit an existing mashup.')
+                .addStringOption(option =>
+                    option.setName('id')
+                    .setDescription('The ID of the mashup to edit.')
+                    .setRequired(true)),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('delete')
+                .setDescription('Delete a mashup.')
+                .addStringOption(option =>
+                    option.setName('id')
+                    .setDescription('The ID of the mashup to delete.')
+                    .setRequired(true)),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('view')
+                .setDescription('View an existing mashup.')
+                .addStringOption(option =>
+                    option.setName('id')
+                    .setDescription('The ID of the mashup to view.')
+                    .setRequired(true)),
+        ),
+    async execute(interaction) {
+        await interaction.deferReply();
+        switch (interaction.options.getSubcommand()) {
+            case 'new':
+                return mashupNew(interaction, interaction.options.getString('tracks'));
+            case 'list':
+                return mashupList(interaction);
+            case 'edit':
+                return mashupEdit(interaction, interaction.options.getString('id'));
+            case 'delete':
+                return mashupDelete(interaction, interaction.options.getString('id'));
+            case 'view':
+                return mashupView(interaction, interaction.options.getString('id'));
+        }
+    },
+};
+
+mashupNew = async (interaction, tracks) => {
+    if (tracks) {
+        tracks = tracks.split(',').map(t => t.trim());
+        tracks = tracks.flatMap(t => searchTrack(getRows(), t));
+    }
+    mash = new Mashup(`${interaction.user.id}-${Date.now()}`, tracks, interaction.user.username, Date.now(), Date.now());
+    await interaction.editReply(await mash.getEmbed());
+}
+
+mashupList = (interaction) => {
+    data = fs.readFileSync(mashupsJson, 'utf8');
+    mashups = JSON.parse(data).filter(m => m.creator === interaction.user.username);
+    if (mashups.length === 0) {
+        return interaction.editReply('You have no mashups yet. Use /mashup new to create one!');
+    } else {
+        const embed = new EmbedBuilder()
+            .setTitle(`${interaction.user.username}'s Mashups`)
+            .setDescription(mashups.map(m => `**${m.title}** - Created on ${new Date(m.createdAt).toLocaleDateString()}`).join('\n'));
+        return interaction.editReply({ embeds: [embed] });
+    }
+}
+
+mashupEdit = (interaction) => {
+    
+}
+
+mashupDelete = (interaction) => {
+    mashup = Mashup.load(interaction.options.getString('id'));
+    if (mashup && mashup.creator === interaction.user.username) {
+        fs.readFile(mashupsJson, 'utf8', (err, data) => {
+            if (err) throw err;
+            if (data) {
+                const mashups = JSON.parse(data);
+                const updatedMashups = mashups.filter(m => m.id !== mashup.id);
+                fs.writeFile(mashupsJson, JSON.stringify(updatedMashups, null, 2), (err) => {
+                    if (err) throw err;
+                    return interaction.reply('Mashup deleted successfully.');
+                });
+            }
+        });
+    } else {
+        return interaction.reply('Mashup not found or you do not have permission to delete it.');
+    }
+}
+
+mashupView = async (interaction) => {
+    mashup = Mashup.load(interaction.options.getString('id'));
+    if (mashup && mashup.creator === interaction.user.username) {
+        return interaction.editReply(await mashup.getEmbed());
+    } else {
+        return interaction.editReply('Mashup not found or you do not have permission to view it.');
+    }
+}
---
+++ resources/modules/mashup.js
@@ -0,0 +1,133 @@
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { generateMashupTitle, generateGenreName } = require.main.require('./resources/modules/llm.js');
+const { pickTrack } = require.main.require('./resources/modules/pickTrack.js');
+const fs = require('fs');
+const interactionCreate = require('../../events/interactionCreate');
+const mashupsJson = './resources/objects/mashups.json';
+
+class Mashup {
+    constructor(id, tracks, creator, createdAt, updatedAt) {
+        this.id = id;
+        this.tracks = [];
+        tracks.forEach(t => this.addTrack(t));
+        this.creator = creator;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.save();
+    }
+
+    static load(id) {
+        const data = fs.readFileSync(mashupsJson, 'utf8');
+        const mashups = JSON.parse(data);
+        const existing = mashups.find(m => m.id === id);
+        if (existing) {
+            return new Mashup(existing.id, existing.tracks, existing.creator, existing.createdAt, existing.updatedAt);
+        } else {
+            return null; // or throw an error idk
+        }
+    }
+
+    async save() {
+        this.updatedAt = Date.now();
+        fs.readFile(mashupsJson, 'utf8', (err, data) => {
+            if (err) throw err;
+            if (data) {
+                const mashups = JSON.parse(data);
+                const existingIndex = mashups.findIndex(m => m.id === this.id);
+            if (existingIndex !== -1) {
+                mashups[existingIndex] = this;
+            } else {
+                mashups.push(this);
+            }
+            fs.writeFile(mashupsJson, JSON.stringify(mashups, null, 2), (err) => {
+                if (err) throw err;
+                console.log(`Mashup saved to ${mashupsJson}`);
+            });
+            } else {
+                fs.writeFile(mashupsJson, JSON.stringify([this], null, 2), (err) => {
+                    if (err) throw err;
+                    console.log(`Mashup saved to new ${mashupsJson}`);
+                });
+            }
+        });
+    }
+
+    deSpreadsheetifyRow(row) {
+        return {
+            Artists: row.Artists,
+            B: row.B,
+            BPM: row.BPM,
+            CC: row.CC,
+            Comp: row.Comp,
+            Date: row.Date,
+            ID: row.ID,
+            Key: row.Key,
+            Label: row.Label,
+            Length: row.Length,
+            Track: row.Track,
+        };
+    }
+
+    addTrack(track) {
+        const cleanTrack = this.deSpreadsheetifyRow(track);
+        if (this.tracks.some(t => t === cleanTrack)) return;
+        this.tracks.push(cleanTrack);
+        this.suggestedTrack = this.deSpreadsheetifyRow(this.suggestTrack());
+        this.save();
+    }
+
+    removeTrack(track) {
+        this.tracks = this.tracks.filter(t => t !== track);
+        this.save();
+    }
+
+    getTracklist() {
+        return this.tracks;
+    }
+
+    async getEmbed() {
+        const embed = new EmbedBuilder()
+            .setTitle(`${this.creator} - ${await generateMashupTitle(this.tracks)}`)
+            .setFooter({ text: `Last updated on ${new Date(this.updatedAt).toLocaleDateString()}`
+        });
+        let actionRow = new ActionRowBuilder();
+
+        const averageBPM = Math.round(this.tracks.reduce((tot, track) => tot + parseInt(track.BPM), 0) / this.tracks.length);
+        const averageKeyID = Math.floor(this.tracks.reduce((tot, track) => tot + getKeyID(track.Key), 0) / this.tracks.length);
+        const averageKey = getMinKey(averageKeyID);
+
+        this.tracks.forEach(track => {
+            let pitchDiff = getKeyID(averageKey) - getKeyID(track.Key);
+            embed.addFields({ name: `${track.Artists} - ${track.Track}`, value: `Key: ${track.Key} (pitch ${pitchDiff >= 0 ? '+' : ''}${pitchDiff}), BPM: ${track.BPM}` });
+        });
+        embed.setDescription(`Last edited on ${new Date(this.updatedAt).toLocaleDateString()}\nGenre: ${await generateGenreName(this.tracks.map(t => t.Label))}\nSuggested Key: ${averageKey}\nSuggested BPM: ${averageBPM}`);
+
+        if (this.suggestedTrack) {
+            embed.addFields({ name: 'Suggested track', value: `${this.suggestedTrack.Artists} - ${this.suggestedTrack.Track} (${this.suggestedTrack.Key}, ${this.suggestedTrack.BPM}bpm)` });
+            let btnAddSuggested = new ButtonBuilder()
+                .setCustomId(`add_suggested_${this.id}`)
+                .setLabel('✨Add suggested')
+                .setStyle(ButtonStyle.Primary);
+            actionRow.addComponents(btnAddSuggested);
+        }
+        let btnEdit = new ButtonBuilder()
+            .setCustomId(`edit_mashup_${this.id}`)
+            .setLabel('✍️Edit')
+            .setStyle(ButtonStyle.Primary);
+        let btnDelete = new ButtonBuilder()
+            .setCustomId(`delete_mashup_${this.id}`)
+            .setLabel('Delete')
+            .setStyle(ButtonStyle.Danger);
+        actionRow.addComponents(btnEdit, btnDelete);
+
+        return { embeds: [embed], components: [actionRow] };
+    }
+
+    suggestTrack() {
+        return pickTrack(this.tracks, getRows());
+    }
+}
+
+module.exports = {
+    Mashup
+}
